ef48147162a5fe50c91c78db8949a485c695a7f2
angelalita77 <anjeanette@gmail.com>
2026-04-20 18:52:42 -0400

    Created youtubeRoutes.mjs and create a route for searching via Youtube API

---
+++ routes/youtubeRoutes.mjs
@@ -0,0 +1,44 @@
+import { Router } from "express";
+import dotenv from 'dotenv';
+
+const router = Router();
+dotenv.config();
+
+router
+.rourter
+.get("/search", async(req, res) => {
+    try{
+        const {q} = req.query;
+
+        if(!q){
+            return res.status(400).json({
+                msg: "Query is requred"
+            })
+        }
+
+        const url = new URL("https://www.googleapis.com/youtube/v3/search");
+
+        url.search = new URLSearchParams({
+            part: "snippet",
+            q,
+            type: "video",
+            maxResults: 10,
+            key: process.env.GOOGLE_API_KEY
+        });
+        const response = await fetch(url);
+        const data = await response.json();
+        
+        const videos = data.items?.filter(
+            (item) => item.id.kind === "youtube#video"
+        );
+        
+        res.json({ videos });
+
+    } catch (err) {
+        console.error("Error fetching YouTube data:", err);
+        res.status(500).json({ msg: err.message})
+    }
+
+});
+
+export default router;
--- server.mjs
+++ server.mjs
@@ -1,7 +1,7 @@
 // Imports
 import express from 'express';
 import dotenv from 'dotenv';
-import mongoose from 'mongoose';
+//import mongoose from 'mongoose';
 import { globalErr, log } from './middleware/middleware.mjs';
 import connectDB from './db/conn.mjs';
 import cors from 'cors';
@@ -24,15 +24,16 @@ connectDB();
 // Middleware
 app.use(cors());
 app.use (express.json());
-app.use(log)
+app.use(log);
 
 
 // Routes
-app.use("/api/register/parent", userRoutes)
+//app.use("/api/register/parent", userRoutes)
 app.use("/api/videos", videoRoutes);
 
 // Global Err Handling
 app.use(globalErr);
+
 // Listener
 app.listen(PORT, () => {
     console.log(`Server Running on PORT: ${PORT}`);
